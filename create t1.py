3a6c07e9636eb86b1d831feb0a10833ee70b6b8c
saraswathy8 <saraswathy.csbs@sairam.edu.in>
2024-07-24 01:42:17 -0700

    Update create t1.py

--- create t1.py
+++ create t1.py
@@ -10,3 +10,4 @@
 saraswathy8/saraswathy8 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
 You can click the Preview link to take a look at your changes.
 --->
+print('hell0')
